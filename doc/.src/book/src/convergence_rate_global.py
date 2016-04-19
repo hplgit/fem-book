06763e07747dc7812075758ef6791d054d18b1d4
Kent-Andre Mardal <kent-and@simula.no>
2016-04-19 12:29:53 +0200

    script and figure

---
+++ doc/.src/book/src/convergence_rate_global.py
@@ -0,0 +1,71 @@
+# check convergence rate of Fourier series and 
+# global polynomials on either an exponential 
+# function or a Gaussian bell.  
+# Plot log-log and do regression / least square . 
+# Discuss the problems with using regression / least square 
+
+
+import sympy as sym
+from approx1D import least_squares 
+import scipy
+import scipy.integrate
+
+
+def sin_series(N): 
+  psi = []
+  for k in range(1,N): 
+    psi_k = sym.sin(sym.pi*k*x)
+    psi.append(psi_k)
+  return psi
+
+def taylor_series(N): 
+  psi = []
+  for k in range(1,N): 
+    psi_k = x**k 
+    psi.append(psi_k)
+  return psi
+
+def series(series_type, N): 
+  if series_type=="Taylor" : return taylor_series(N)
+  elif series_type=="sin"  : return sin_series(N)
+  else: print "series type unknown " # sys.exit(0)
+
+def convergence_rate_analysis(series_type): 
+  Ns =[2, 4, 8, 16, 32, 64]
+  norms = []
+  for N in Ns: 
+
+    psi = series(series_type, N)
+    u, c = least_squares(gauss_bell, psi, Omega, False) 
+
+    error2 = sym.lambdify([x], (gauss_bell - u)**2)
+    L2_norm = scipy.integrate.quad(error2, Omega[0], Omega[1])  
+    L2_norm = scipy.sqrt(L2_norm)
+
+    print "L2_norm ", L2_norm  
+    norms.append(L2_norm[0])
+
+  print "Ns ", Ns
+  print "norms ", norms 
+  return Ns, norms 
+
+
+Omega = [0, 1]
+x = sym.Symbol("x")
+gauss_bell = sym.exp(-(x-0.5)**2) - sym.exp(-1)
+
+
+import pylab
+series_types = ["Taylor", "sin"]
+for series_type in series_types: 
+  Ns, norms = convergence_rate_analysis(series_type)
+  pylab.loglog(Ns, norms)
+
+pylab.legend(series_types)
+pylab.show()
+
+
+
+
+
+
