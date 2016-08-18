ab4f2cce6b7426c9f9d84addb0f5b3baf2ed11a7
Hans Petter Langtangen <hpl@simula.no>
2016-08-17 23:30:26 -0700

    merge

---
+++ doc/.src/book/dotxt/edit_nonlin.sh
@@ -0,0 +1,5 @@
+# nonline*.do.txt can be copied from fdm-book (after proof reading for instance)
+# but then we need some editing:
+doconce replace fig-nonlin/ fig/ nonlin*.do.txt
+doconce replace src-nonlin/ src/ nonlin*.do.txt
+doconce replace src_nonlin fem_src nonlin*.do.txt
