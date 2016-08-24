41f1e79cd5e0c46cce26c95bc379ca9ae3fb3a23
Kent-Andre Mardal <kent-and@simula.no>
2016-08-24 22:01:19 +0200

    minor things to make polychaos compile

--- doc/.src/book/src/ex_varform1D.py
+++ doc/.src/book/src/ex_varform1D.py
@@ -49,7 +49,7 @@ def case0(f, N=3):
 
     Omega = [0, 1]
 
-    u_bar = solve(integrand_lhs, integrand_rhs, phi, Omega,
+    u_bar = solver(integrand_lhs, integrand_rhs, phi, Omega,
                   verbose=True, symbolic=True)
     u = B + u_bar
     print 'solution u:', sym.simplify(sym.expand(u))
@@ -63,7 +63,7 @@ def case0(f, N=3):
     C1, C2 = sym.symbols('C1 C2')
     u_e = -f2 + C1*x + C2
     # Find C1 and C2 from the boundary conditions u(0)=0, u(1)=1
-    s = sym.solve([u_e.subs(x,0) - 1, u_e.subs(x,1) - 0], [C1, C2])
+    s = sym.solver([u_e.subs(x,0) - 1, u_e.subs(x,1) - 0], [C1, C2])
     # Form the exact solution
     u_e = -f2 + s[C1]*x + s[C2]
     print 'analytical solution:', u_e
@@ -107,9 +107,9 @@ def case1(N, basis='sines'):
     Omega = [0, 1]
     phi = phi_factory(basis, N, 2)
     u = {}  # dict of solutions corresponding to different methods
-    u['LS'] = solve(integrand_lhs_LS, integrand_rhs_LS, phi, Omega)
-    u['G1'] = solve(integrand_lhs_G1, integrand_rhs_G1, phi, Omega)
-    u['G2'] = solve(integrand_lhs_G2, integrand_rhs_G2, phi, Omega)
+    u['LS'] = solver(integrand_lhs_LS, integrand_rhs_LS, phi, Omega)
+    u['G1'] = solver(integrand_lhs_G1, integrand_rhs_G1, phi, Omega)
+    u['G2'] = solver(integrand_lhs_G2, integrand_rhs_G2, phi, Omega)
     u['exact'] = x*(D + L - x)
     # Test different collocation points
     points = []
@@ -174,7 +174,7 @@ def case2(N):
     phi = phi_factory('poly3', N, 1)
     #phi = phi_factory('Lagrange', N, 1)
     print phi[0]
-    u = {'G1': solve(integrand_lhs, integrand_rhs, phi, Omega,
+    u = {'G1': solver(integrand_lhs, integrand_rhs, phi, Omega,
                      boundary_lhs, boundary_rhs, verbose=True) + B,
          'exact': u_e}
     print 'numerical solution:', u['G1']
@@ -274,7 +274,7 @@ def case3(N, a=1, a_symbols={}, f=0, f_symbols={},
     dBdx = sym.diff(B, x)
 
     verbose = True if symbolic else False
-    phi_sum = solve(integrand_lhs, integrand_rhs, phi, Omega,
+    phi_sum = solver(integrand_lhs, integrand_rhs, phi, Omega,
                     boundary_lhs, boundary_rhs, verbose=verbose,
                     symbolic=symbolic)
     print 'sum c_j*phi_j:', phi_sum
