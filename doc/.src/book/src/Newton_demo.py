671b2dce0350967eb65aa2d6ef8afe4fd068f8e8
Kent-Andre Mardal <kent-and@simula.no>
2018-08-02 13:44:20 +0200

    fixed the demo

--- doc/.src/book/src/Newton_demo.py
+++ doc/.src/book/src/Newton_demo.py
@@ -1,5 +1,5 @@
-
-from math import * 
+import matplotlib.pyplot as plt 
+from numpy import * 
 
 from sys import argv 
 if len(argv) != 6: 
@@ -11,19 +11,30 @@ x0 = float(argv[3])
 xmin = float(argv[4])
 xmax = float(argv[5])
 
-
-
 i = 0
 tol = 1.0e-9 
 maxit = 100 
-f = 1 
 x = x0
+f = eval(f_str, vars())
+dfdx = eval(dfdx_str, vars())
+xs = []
+fs = []
+xs.append(x)
+fs.append(f)
+print "x=%.3e   f=%.3e   dfdx=%.3e " %  (x, f, dfdx)  
 while abs(f) > tol and i <= maxit and x > xmin and x < xmax :  
+  x = x0 - f/dfdx
   f = eval(f_str, vars())
   dfdx = eval(dfdx_str, vars())
-  x = x0 - f/dfdx
-
-  x0 = x 
-  print "x=%.3e   f=%.3e   dfdx=%.3e " %  (x, f, dfdx)  
+  xs.append(x0)
+  fs.append(f)
   i = i+1 
+  print "x=%.3e   f=%.3e   dfdx=%.3e " %  (x, f, dfdx)  
 
+x = arange(xmin, xmax, (xmax-xmin)/100.0)
+f = eval(f_str, vars())
+
+plt.plot(x, f, "g")  
+plt.plot(xs, fs, "bo")  
+plt.plot(xs, fs, "b")  
+plt.show()
