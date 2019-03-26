8740fc413f561669a1bc1b6405c21ba2568cb51c
Kent-Andre Mardal <kent-and@simula.no>
2019-03-26 18:18:32 +0100

    update

--- doc/.src/book/copy4springer.sh
+++ doc/.src/book/copy4springer.sh
@@ -1,7 +1,7 @@
 
 
 mkdir springer 
-cp -r book.tex papers.bib svmonodo.cls newcommands_keep.tex t4do.sty fig latex_figs mov fem-book-4print.pdf springer/.  
+cp -r book.tex book.do.txt papers.bib svmonodo.cls newcommands_keep.tex t4do.sty fig latex_figs mov fem-book-4print.pdf dotxt src springer/.  
 tar -cf springer.tar springer  
 gzip springer.tar 
 
